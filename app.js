33f9a55610615debf5e1e259343dad89bbb431fb
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-08-14 11:26:39 +0700

    delete database 🤣

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
+const Pool = require("pg").Pool;
+const shortid = require("shortid");
+
+require("dotenv").config();
+
+const credentials = {
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.PASSWORD,
+  port: 6543,
+};
+
+const pool = new Pool(credentials);
+
+const getUrl = (request, response) => {
+  pool.query("SELECT * FROM url ", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+const getShortUrl = (request, response) => {
+  pool.query(
+    "SELECT * FROM url where short_url = $1",
+    [request.params.shortUrl],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      // return response.json(results.rows[0]);
+      if (results.rows[0] != null) {
+        return response.json({ full_url: results.rows[0].full_url });
+      }
+      return response.json({ full_url: "" });
+    }
+  );
+};
+const postUrl = (request, response) => {
+  const fullUrl = request.body.fullUrl;
+  const shortUrl = shortid();
+  pool.query(
+    "INSERT INTO url(full_url, short_url) VALUES ($1, $2) RETURNING * ",
+    [fullUrl, shortUrl],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json({ short_url: results.rows[0].short_url });
+    }
+  );
+};
+
+module.exports = { getUrl, postUrl, getShortUrl };
+
 const express = require("express");
-const db = require("./database.js");
+
+// const db = require("./database").default;
 
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUI = require("swagger-ui-express");
@@ -50,16 +107,16 @@ app.use(express.static("public"));
 //  *         description: the list of all URLs
 //  */
 app.get("/", async (req, res) => {
-  db.getUrl(req, res);
+  getUrl(req, res);
 });
 
 app.get("/:shortUrl", async (req, res) => {
-  db.getShortUrl(req, res);
+  getShortUrl(req, res);
 });
 
 app.post("/", async (req, res) => {
   if (req.body.fullUrl.includes("http") == true) {
-    db.postUrl(req, res);
+    postUrl(req, res);
   } else {
     res.status(401).json({ message: "provide valid links !" });
   }
